73f39f9c9b6fb170dd5c6b7a00e4e25912e9eb79
WillRock19 <williamporto_14@hotmail.com>
2020-10-18 13:20:24 -0300

    Configuring my api test to expect an json

--- test/integration_api/api_financeira/ballance_api_test.clj
+++ test/integration_api/api_financeira/ballance_api_test.clj
@@ -1,8 +1,9 @@
 (ns api-financeira.ballance-api-test
     (:require   [api-financeira.aux-functions :refer :all]
+                [cheshire.core :as json]
                 [midje.sweet :refer :all]))
 
 (against-background [(before :facts (start-server server-port))
                      (after  :facts (stop-server))]
   (fact "The initial ballance is zero" :acceptance
-    (content-of-request "/ballance") => "0"))
+    (json/parse-string (content-of-request "/ballance")) => {"saldo" 0}))
