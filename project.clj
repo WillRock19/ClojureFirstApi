57fbe98788e5ec5c5ce0b408bb9499a2012529c7
WillRock19 <williamporto_14@hotmail.com>
2020-10-18 10:00:07 -0300

    Adding ring-jetty-adapter to start a server and run integration tests, and ring-core 'cause it is one of it depedencies

--- project.clj
+++ project.clj
@@ -12,4 +12,6 @@
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.2"]
+                        [ring/ring-core "1.7.1"]
+                        [ring/ring-jetty-adapter "1.7.1"]
                         [midje "1.9.6"]]}})
