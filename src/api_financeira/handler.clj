9ab7ca55b3c76ec16cfe10f589d071bcaf8e159f
WillRock19 <williamporto_14@hotmail.com>
2020-10-18 13:23:33 -0300

    Adjusting endpoint to return a json

--- src/api_financeira/handler.clj
+++ src/api_financeira/handler.clj
@@ -1,11 +1,12 @@
 (ns api-financeira.handler
-  (:require [compojure.core :refer :all]
+  (:require [cheshire.core :as json]
+            [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
-  (GET "/ballance" [] "0")
+  (GET "/ballance" [] (json/generate-string { :saldo 0 }))
   (route/not-found "The resource does not exist!"))
 
 (def app
